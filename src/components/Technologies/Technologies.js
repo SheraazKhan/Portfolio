bfb3707bd82dfd12c74d89de3f0fd49f0a6a7c33
Sheraaz <57414706+SheraazKhan@users.noreply.github.com>
2025-07-15 19:52:17 -0400

    Initial portfolio upload

---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider /> 
+    <br />
+    <SectionTitle>Technologies</SectionTitle> 
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      From Back-end to Design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node and databases
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Figma <br />
+            Adobe Photoshop
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+        <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Networking</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Wireshark
+
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+        <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Languages</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Python <br />
+            C# <br />
+            JavaScript <br />
+            Java <br />
+            SQL <br />
+            Linux
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+        <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Certification</ListTitle>
+          <ListParagraph>
+            Completed <br />
+            COMPTIA Security +  <br />
+            Google Cybersecurity Professional Certificate <br />
+            COMPTIA A+ (In Progres) 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
+
+export default Technologies;
---
+++ src/constants/constants.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: 'PacketSniffer',
+    description: "A real-time network packet sniffer with a modern graphical user interface (GUI) built using Python, Scapy, and ttkbootstrap. It captures and displays UDP, DNS, TCP, ARP, and ICMP packets on your network interface, with live filtering, logging, and export capabilities.",
+      image: '/images/Packetsniffer1.png',
+      tags: ['Python'],
+    source: 'https://google.com',
+    visit: 'https://google.com',
+    id: 0,
+  },
+  {
+    title: 'QuickCart',
+    description:"QuickCart is a fully functional eCommerce web application. It allows users to browse, add, and purchase products while giving administrators full control over product management.",
+    image: '/images/Quickcart.png',
+    tags: ['React', 'JavaScript, MongoDB, Node, Express'],
+    source: 'https://quickcart-frontend-dn9d.onrender.com',
+    visit: 'https://google.com',
+    id: 1,
+  },
+  // {
+  //   title: 'WebRTC App',
+  //   description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+  //     image: '/images/3.jpg',
+  //     tags: ['React', 'WebRTC'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 2,
+  // },
+  // {
+  //   title: 'Unichat',
+  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: '/images/4.jpg',
+  //   tags: ['React', 'ChatEngine', 'Firebase'],
+  //   source: 'https://google.com',
+  //   visit: 'https://google.com',
+  //   id: 3,
+  // },
+];
+
+export const TimeLineData = [
+  // { year: 2017, text: 'Started my journey', },
+  // { year: 2018, text: 'Worked as a freelance developer', },
+  // { year: 2019, text: 'Founded JavaScript Mastery', },
+  // { year: 2020, text: 'Shared my projects with the world', },
+  // { year: 2021, text: 'Started my own platform', },
+];
